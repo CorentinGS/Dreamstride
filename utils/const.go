bbbdd187f5d501fca6ec359212df40201f13ddc1
Quentin <79152815+Moustik06@users.noreply.github.com>
2022-12-15 21:47:23 +0100

    random fix (#4)

--- utils/const.go
+++ utils/const.go
@@ -1,11 +1,12 @@
 package utils
 
 const (
-	GITHUB  = "https://github.com/Moustik06/Dreamstride"
-	VERSION = "0.0.1"
-	GREEN   = 0x00ff00
-	RED     = 0xff0000
-	BLUE    = 0x0000ff
-	ORANGE  = 0xffa500
-	PURPLE  = 0x800080
+	GITHUB    = "https://github.com/Moustik06/Dreamstride"
+	VERSION   = "0.0.1"
+	SERVER_ID = "955192188332023819"
+	GREEN     = 0x00ff00
+	RED       = 0xff0000
+	BLUE      = 0x0000ff
+	ORANGE    = 0xffa500
+	PURPLE    = 0x800080
 )
