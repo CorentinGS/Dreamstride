6b20b86d9cd5eb82890d0fffe8b59e750fda6080
Moustik06 <starlinator06@live.fr>
2022-12-16 17:38:02 +0100

    mute

--- commands/mute.go
+++ commands/mute.go
@@ -8,15 +8,11 @@ import (
 )
 
 func timeoutUser(s *discordgo.Session, userID string, until time.Duration) bool {
-	// Initialise une session HTTP
-
 	url := "https://discord.com/api/v9/guilds/" + utils.SERVER_ID + "/members/" + userID
 	timeout := time.Now().Add(until)
-
 	json := map[string]string{
 		"communication_disabled_until": timeout.Format(time.RFC3339),
 	}
-
 	_, err := s.RequestWithBucketID("PATCH", url, json,
 		discordgo.EndpointGuildMember(utils.SERVER_ID, userID))
 	if err != nil {
@@ -28,21 +24,21 @@ func timeoutUser(s *discordgo.Session, userID string, until time.Duration) bool 
 func MuteCommand() func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		user := i.ApplicationCommandData().Options[0].UserValue(s).ID
+		userName := i.ApplicationCommandData().Options[0].UserValue(s).Username
 		times, _ := strconv.Atoi(i.ApplicationCommandData().Options[1].StringValue())
 
-		//mute the user
 		if timeoutUser(s, user, time.Duration(times)*time.Minute) {
 			_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: "User has been muted for " + string(rune(times)) + " minutes",
+					Content: userName + " has been muted for " + i.ApplicationCommandData().Options[1].StringValue() + " minutes",
 				},
 			})
 		} else {
 			_ = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
-					Content: "Error muting user",
+					Content: "Error muting " + userName,
 				},
 			})
 		}
