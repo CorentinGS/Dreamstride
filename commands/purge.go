314e52e37991a4bfa9c111f94a43bc3a7c8e2718
Moustik06 <starlinator06@live.fr>
2022-12-19 21:21:12 +0100

    testing things

--- commands/purge.go
+++ commands/purge.go
@@ -1,13 +1,45 @@
 package commands
 
 import (
+	"Dreamstride/utils"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"sync"
+	"time"
 )
 
+const rateLimit = 2 // rate limit of 2 requests per second
+
+type rateLimiter struct {
+	tokens int
+	tick   *time.Ticker
+	mutex  sync.Mutex
+}
+
+func newRateLimiter(rate int) *rateLimiter {
+	return &rateLimiter{
+		tokens: rate,
+		tick:   time.NewTicker(time.Second / time.Duration(rate)),
+	}
+}
+
+func (r *rateLimiter) limit() {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	if r.tokens > 0 {
+		r.tokens--
+		return
+	}
+
+	r.mutex.Unlock()
+	<-r.tick.C
+	r.mutex.Lock()
+	r.tokens--
+}
 func PurgeCommand() func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+
 		numMessage := i.ApplicationCommandData().Options[0].IntValue()
 
 		var wg sync.WaitGroup
@@ -16,14 +48,18 @@ func PurgeCommand() func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		var optionalUser *discordgo.User
 		if len(i.ApplicationCommandData().Options) > 1 {
 			optionalUser = i.ApplicationCommandData().Options[1].UserValue(s)
+			utils.Log(i.User.Username, i.User.ID, "purged messages from", optionalUser.Username, optionalUser.ID)
+		} else {
+			utils.Log(i.User.Username, i.User.ID, "purged messages")
 		}
-
 		messages, _ := s.ChannelMessages(i.ChannelID, int(numMessage), "", "", "")
 
+		rateLimiter := newRateLimiter(rateLimit)
 		for _, message := range messages {
 			message := message
 			go func(messageID string) {
 				defer wg.Done()
+				rateLimiter.limit()
 				if optionalUser != nil {
 					if message.Author.ID == optionalUser.ID {
 						_ = s.ChannelMessageDelete(i.ChannelID, messageID)
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,12 @@
 package utils
+
+import (
+	"log"
+	"runtime"
+)
+
+func Log(caller string, id string, messages ...string) {
+	pc, _, _, _ := runtime.Caller(1)
+	callingFunc := runtime.FuncForPC(pc).Name()
+	log.Println(callingFunc, "called by", caller, "with ID", id, messages)
+}
