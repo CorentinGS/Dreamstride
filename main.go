a602fc7db6989b0428044022909d590fd0c68510
Moustik06 <starlinator06@live.fr>
2022-12-21 23:15:03 +0100

    react

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func sendSupportEmbed(s *discordgo.Session) {
 	}
 	s.AddHandler(func(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
 		if r.MessageID == msg.ID && r.Emoji.Name == "📩" {
-			st, err := s.UserChannelCreate(r.MessageID)
+			st, err := s.UserChannelCreate(r.Member.User.ID)
 			if err != nil {
 				log.Println("Error while creating channel ", err)
 			}
