339e56d8f7afae570eeaa2653c9a3af5251ee4ce
Moustik06 <starlinator06@live.fr>
2022-12-22 19:26:26 +0100

    join handler

--- utils/const.go
+++ utils/const.go
@@ -9,4 +9,9 @@ const (
 	BLUE      = 0x0000ff
 	ORANGE    = 0xffa500
 	PURPLE    = 0x800080
+
+	// link used for joining embed
+	WELCOME_LINK = "https://cdn.discordapp.com/attachments/955192189263159367/1054945712930046002/Welcome_to_Dreamstride.png"
+	WELCOME_ICON = "https://cdn.discordapp.com/attachments/1033771925052338217/1054934057995227146/Dreamstride_Icon.png"
+	WELCOME_CHAN = "955192189032493148"
 )
