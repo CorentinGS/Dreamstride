288da5fcadaef2a531ab92c003455afa49a5adbf
Moustik06 <starlinator06@live.fr>
2023-02-23 19:49:48 +0100

    say command

--- commands/say.go
+++ commands/say.go
@@ -7,7 +7,7 @@ import (
 func SayCommand() func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	return func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if i.Member.User.ID == "813286644652179467" || i.Member.User.ID == "219472739109568518" {
-			channel := i.ApplicationCommandData().Options[0].StringValue()
+			channel := i.ApplicationCommandData().Options[0].ChannelValue(s).ID
 			/*Prepare an embed to send*/
 			embed := &discordgo.MessageEmbed{
 				Description: i.ApplicationCommandData().Options[1].StringValue(),
