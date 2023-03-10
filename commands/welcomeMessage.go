bb4b8a1cbb9953cfe151ca8fa834f1c1b62a1adb
qescobar <starlinator06@live.fr>
2023-03-10 21:19:27 +0100

    new welcome msg

--- commands/welcomeMessage.go
+++ commands/welcomeMessage.go
@@ -14,11 +14,30 @@ func WelcomeImageCommand() func(s *discordgo.Session, i *discordgo.InteractionCr
 				Embeds: []*discordgo.MessageEmbed{
 					{
 						Title:       "Welcome Image",
-						Description: "The welcome image has been set to: " + utils.WELCOME_LINK,
+						Description: "The welcome image has been set to: ",
 						Color:       utils.GREEN,
 					},
 				},
 			},
 		})
+		embed := &discordgo.MessageEmbed{
+			Title: "Welcome to the server !<a:DS_strawberry:1081335566265761842>",
+			Description: "︶︶︶✿︶♡︶꒷꒦︶︶✿︶︶\n" +
+				"<a:DS_heart:1081315143398461461> : <#955192188776616081>\n" +
+				"<a:DS_heart:1081315143398461461> : <#955192188776616085>\n" +
+				"<a:DS_heart:1081315143398461461> : <#955192188776616082>\n" +
+				"<a:DS_heart:1081315143398461461> : <#995805735235620864>\n" +
+				"<a:DS_heart:1081315143398461461> : <#995823153567768576>\n" +
+				"︶︶︶✿︶♡︶꒷꒦︶︶✿︶︶\n" +
+				"We hope you enjoy your stay !<a:DS_FloatingHeart:955580787909087322>\n",
+			Color: 0xDF73F5,
+			Image: &discordgo.MessageEmbedImage{
+				URL: utils.WELCOME_LINK,
+			},
+			Thumbnail: &discordgo.MessageEmbedThumbnail{
+				URL: utils.WELCOME_ICON,
+			},
+		}
+		_, _ = s.ChannelMessageSendEmbed(i.ChannelID, embed)
 	}
 }
